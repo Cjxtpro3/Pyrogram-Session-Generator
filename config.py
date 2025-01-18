fc475675943948fbb3016e068370de033b41130c
Cjxtpro3 <152521877+Cjxtpro3@users.noreply.github.com>
2025-01-18 09:26:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "23147177")
-API_HASH = getenv("API_HASH", "6c1b34bf3c56b9957aab7da8a0dd3482")
-BOT_TOKEN = getenv("BOT_TOKEN", "7653418941:AAERQ7B36PfCIu2GNaZEg90sAr0Ru21ziD8")
-LOG_GROUP = int(getenv("LOG_GROUP", "savebotuser2")
+API_ID = "23147177"
+API_HASH = "6c1b34bf3c56b9957aab7da8a0dd3482"
+BOT_TOKEN = "7653418941:AAERQ7B36PfCIu2GNaZEg90sAr0Ru21ziD8"
+LOG_GROUP = "savebotuser2"
